ab00c60642363751ee328f50499c5d02f58f5271
Ran Pol <rnd809@gmail.com>
2022-07-09 01:32:55 -0400

    Fix the Session Login Bug to Redirect User To OrigionalUrl

---
+++ testing.js
@@ -0,0 +1,18 @@
+app.use((req, res, next) => {
+    if (!['/login', '/'].includes(req.originalUrl)) {
+        req.session.returnTo = req.originalUrl;
+    }
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+});
+and in middleware.js update the isLoggedIn middleware to be:
+
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.flash('error', 'You must be signed in first!');
+        return res.redirect('/login');
+    }
+    next();
+}
