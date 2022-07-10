28c7deda6f8991c0bc96a7cdee071636b8215206
Ran Pol <rnd809@gmail.com>
2022-07-10 01:46:14 -0400

    Restructure the Routes utilize Router.route

--- routes/campground.js
+++ routes/campground.js
@@ -9,14 +9,11 @@ const { isLoggedIn,
     isAuthor } = require('../middleware')
 
 
+// Chaining Router Routes. This is use to chain together http vers that share similar path
 
-////////CRUD: => INDEX    
-// ////API ENDPOINT: =>  /products
-////////HTTP VERB: => GET
-// ////PURPOSE: => Display a list of all products
-////////MONGOOSE METHOD: => Product.find()
-router.get('/', campControllers.index)
-
+router.route('/')
+    .get(campControllers.index) ////////CRUD: => INDEX
+    .post(isLoggedIn, validateCampground, campControllers.postNewCampground) ////////CRUD: => Create    
 
 ////////CRUD: => New    
 // ////API ENDPOINT: =>  /products/new
@@ -26,20 +23,11 @@ router.get('/', campControllers.index)
 router.get('/new', isLoggedIn, campControllers.getNewCampground)
 
 
-////////CRUD: => Create     
-// ////API ENDPOINT: =>  /products
-////////HTTP VERB: => POST
-// ////PURPOSE: => Add a new product to the database, redirect somewhere
-////////MONGOOSE METHOD: => Product.create() or Product.save()
-router.post('/', isLoggedIn, validateCampground, campControllers.postNewCampground)
+router.route('/:id')
+    .get(campControllers.showCampground) ////////CRUD: => SHOW   
+    .put(isLoggedIn, isAuthor, validateCampground, campControllers.updateCampground) ////////CRUD: => UPDATE
+    .delete(isLoggedIn, isAuthor, campControllers.deleteCampground) ////////CRUD: => DELETE
 
-
-////////CRUD: => SHOW     
-// ////API ENDPOINT: =>  /products/:id
-////////HTTP VERB: => GET
-// ////PURPOSE: => Show information about one product
-////////MONGOOSE METHOD: => Product.findById()
-router.get('/:id', campControllers.showCampground)
 
 
 ////////CRUD: => EDIT   
@@ -50,19 +38,64 @@ router.get('/:id', campControllers.showCampground)
 router.get('/:id/edit', isLoggedIn, isAuthor, campControllers.editCampground);
 
 
-////////CRUD: => UPDATE  
-// ////API ENDPOINT: =>  /products/:id
-////////HTTP VERB: => PUT
-// ////PURPOSE: => Update a particular product's data then redirect somewhere
-////////MONGOOSE METHOD: =>  Product.findByIdAndUpdate()
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, campControllers.updateCampground);
-
-
-////////CRUD: => DELETE
-// ////API ENDPOINT: =>  /products/:id
-////////HTTP VERB: => DELTE
-// ////PURPOSE: => Delete a particular product's data then redirect somewhere
-////////MONGOOSE METHOD: =>  Product.findByIdAndDelete()
-router.delete('/:id', isLoggedIn, isAuthor, campControllers.deleteCampground)
-
 module.exports = router;
+
+
+
+
+// ////////CRUD: => INDEX    
+// // ////API ENDPOINT: =>  /products
+// ////////HTTP VERB: => GET
+// // ////PURPOSE: => Display a list of all products
+// ////////MONGOOSE METHOD: => Product.find()
+// router.get('/', campControllers.index)
+
+
+// ////////CRUD: => New    
+// // ////API ENDPOINT: =>  /products/new
+// ////////HTTP VERB: => GET
+// // ////PURPOSE: => Display form to add a new product
+// ////////MONGOOSE METHOD: => N/A   
+// router.get('/new', isLoggedIn, campControllers.getNewCampground)
+
+
+// ////////CRUD: => Create     
+// // ////API ENDPOINT: =>  /products
+// ////////HTTP VERB: => POST
+// // ////PURPOSE: => Add a new product to the database, redirect somewhere
+// ////////MONGOOSE METHOD: => Product.create() or Product.save()
+// router.post('/', isLoggedIn, validateCampground, campControllers.postNewCampground)
+
+
+// ////////CRUD: => SHOW     
+// // ////API ENDPOINT: =>  /products/:id
+// ////////HTTP VERB: => GET
+// // ////PURPOSE: => Show information about one product
+// ////////MONGOOSE METHOD: => Product.findById()
+// router.get('/:id', campControllers.showCampground)
+
+
+// ////////CRUD: => EDIT   
+// // ////API ENDPOINT: =>  /products/:id/edit
+// ////////HTTP VERB: => GET
+// // ////PURPOSE: => Show edit form for one product
+// ////////MONGOOSE METHOD: => Product.findById()
+// router.get('/:id/edit', isLoggedIn, isAuthor, campControllers.editCampground);
+
+
+// ////////CRUD: => UPDATE  
+// // ////API ENDPOINT: =>  /products/:id
+// ////////HTTP VERB: => PUT
+// // ////PURPOSE: => Update a particular product's data then redirect somewhere
+// ////////MONGOOSE METHOD: =>  Product.findByIdAndUpdate()
+// router.put('/:id', isLoggedIn, isAuthor, validateCampground, campControllers.updateCampground);
+
+
+// ////////CRUD: => DELETE
+// // ////API ENDPOINT: =>  /products/:id
+// ////////HTTP VERB: => DELTE
+// // ////PURPOSE: => Delete a particular product's data then redirect somewhere
+// ////////MONGOOSE METHOD: =>  Product.findByIdAndDelete()
+// router.delete('/:id', isLoggedIn, isAuthor, campControllers.deleteCampground)
+
+// module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,24 +3,45 @@ const router = express.Router();
 
 const passport = require('passport')
 const usersControllers = require('../controllers/users')
-// Register//GET
-router.get('/register', usersControllers.getRegister);
 
-// Register//POST
-router.post('/register', usersControllers.postRegister);
+// Router.route Version
 
+//Register
+router.route('/register')
+    .get(usersControllers.getRegister)// Register//GET
+    .post(usersControllers.postRegister)// Register//POST
 
-// Login//GET
-router.get('/login', usersControllers.getLogin);
-
-// Login//POST
-router.post('/login', passport.authenticate('local',
-    {
-        failureFlash: true, failureRedirect: '/login',
-        keepSessionInfo: true
-    }), usersControllers.postLogin);
+//Login
+router.route('/login')
+    .get(usersControllers.getLogin)// Login//GET
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true }), usersControllers.postLogin) // Login//POST
 
 // Logout
 router.get('/logout', usersControllers.getLogout);
 
 module.exports = router;
+
+
+
+
+// // Register//GET
+// router.get('/register', usersControllers.getRegister);
+
+// // Register//POST
+// router.post('/register', usersControllers.postRegister);
+
+
+// // Login//GET
+// router.get('/login', usersControllers.getLogin);
+
+// // Login//POST
+// router.post('/login', passport.authenticate('local',
+//     {
+//         failureFlash: true, failureRedirect: '/login',
+//         keepSessionInfo: true
+//     }), usersControllers.postLogin);
+
+// // Logout
+// router.get('/logout', usersControllers.getLogout);
+
+// module.exports = router;
