9e8cf89d31952e381662d795eccbc240ecafd6ba
Ran Pol <rnd809@gmail.com>
2022-07-15 08:52:07 -0400

    Fix bug that prevents the pop up

--- public/js/map.js
+++ public/js/map.js
@@ -1,15 +1,18 @@
-
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: mapCoordinates,
+    center: camp.geometry.coordinates,
     zoom: 9
 });
 
 const marker = new mapboxgl.Marker()
-    .setLngLat(mapCoordinates)
+    .setLngLat(camp.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h4>${camp.title}</h4><p>${camp.location}</p>`
+            )
+    )
     .addTo(map);
 
-
-console.log(mapCoordinates);
